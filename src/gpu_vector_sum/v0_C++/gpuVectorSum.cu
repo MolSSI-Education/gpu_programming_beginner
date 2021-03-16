e343eb057fcd5075dd1d99d0601b45f89024acf4
SinaMostafanejad <m_mostafanejad@hotmail.com>
2021-03-15 21:57:29 -0400

    start adding C++ codes for exercises

---
+++ src/gpu_vector_sum/v0_C++/gpuVectorSum.cu
@@ -0,0 +1,159 @@
+/*================================================*/
+/*================ gpuVectorSum.cu ===============*/
+/*================================================*/
+#include <stdlib.h>
+#include <stdio.h>
+#include <iostream>
+#include <iomanip>
+#include <stdbool.h>
+#include <time.h>
+#include <sys/time.h>
+#include <cuda_runtime.h>
+
+/*************************************************/
+inline double chronometer() {
+    struct timezone tzp;
+    struct timeval tp;
+    int tmp = gettimeofday(&tp, &tzp);
+    return ((double)tp.tv_sec + (double)tp.tv_usec * 1.e-6);
+}
+/*-----------------------------------------------*/
+void dataInitializer(float *inputArray, int size) {
+    /* Generating float-type random numbers 
+     * between 0.0 and 1.0
+     */
+    time_t t;
+    srand( (unsigned int) time(&t) );
+
+    for (int i = 0; i < size; i++) {
+        inputArray[i] = ( (float)rand() / (float)(RAND_MAX) ) * 1.0;
+    }
+
+    return;
+}
+/*-----------------------------------------------*/
+void arraySumOnHost(float *A, float *B, float *C, const int size) {
+    for (int i = 0; i < size; i++) {
+        C[i] = A[i] + B[i];
+    }
+}
+/*-----------------------------------------------*/
+__global__ void arraySumOnDevice(float *A, float *B, float *C, const int size) {
+    int idx = blockIdx.x * blockDim.x + threadIdx.x;
+    if (idx < size) { 
+        C[idx] = A[idx] + B[idx];
+    }
+}
+/*-----------------------------------------------*/
+void arrayEqualityCheck(float *hostPtr, float *devicePtr, const int size) {
+    double tolerance = 1.0E-8;
+    bool isEqual = true;
+
+    for (int i = 0; i < size; i++) {
+        if (abs(hostPtr[i] - devicePtr[i]) > tolerance) {
+            isEqual = false;
+            std::cout << "Arrays are NOT equal because:" << std::endl;
+            std::cout << "at " << i << "th index: hostPtr[" << i << "] = " <<
+            std::setprecision(16) << hostPtr[i] << " and devicePtr[" << i << "] = "
+            << devicePtr[i] << std::endl;
+            break;
+        }
+    }
+
+    if (isEqual) {
+        std::cout << "Arrays are equal.\n" << std::endl;
+    }
+}
+/*************************************************/
+int main(int argc, char **argv) {
+    std::cout << "Kicking off " 
+    << argv[0] << "\n" << std::endl;
+
+    /* Device setup */
+    int deviceIdx = 0;
+    cudaSetDevice(deviceIdx);
+
+    /* Device properties */
+    cudaDeviceProp deviceProp;
+    cudaGetDeviceProperties(&deviceProp, deviceIdx);
+    std::cout << "GPU device " << deviceProp.name <<
+    " with index (" << deviceIdx << ") is set!\n" << 
+    std::endl;
+/*-----------------------------------------------*/
+    /* Fixing the vector size to 1 * 2^24 = 16777216 (64 MB) */
+    int vecSize = 1 << 24;
+    size_t vecSizeInBytes = vecSize * sizeof(float);
+    std::cout << "Vector size: " << vecSize << 
+    " floats (" << vecSizeInBytes/1024/1024 << " MB)\n" << 
+    std::endl;
+
+    /* Memory allocation on the host */
+    float *h_A  = new float[vecSizeInBytes];
+    float *h_B  = new float[vecSizeInBytes];
+    float *hostPtr   = new float[vecSizeInBytes]();
+    float *devicePtr = new float[vecSizeInBytes]();
+
+    double tStart, tElapsed;
+
+    /* Vector initialization on the host */
+    tStart = chronometer();
+    dataInitializer(h_A, vecSize);
+    dataInitializer(h_B, vecSize);
+    tElapsed = chronometer() - tStart;
+    std::cout << "Elapsed time for dataInitializer: "
+    << tElapsed <<  " second(s)" << std::endl;
+
+    /* Vector summation on the host */
+    tStart = chronometer();
+    arraySumOnHost(h_A, h_B, hostPtr, vecSize);
+    tElapsed = chronometer() - tStart;
+    printf("Elapsed time for arraySumOnHost: %f second(s)\n", tElapsed);
+/*-----------------------------------------------*/
+    /* (Global) memory allocation on the device */
+    float *d_A, *d_B, *d_C;
+    cudaMalloc((float**)&d_A, vecSizeInBytes);
+    cudaMalloc((float**)&d_B, vecSizeInBytes);
+    cudaMalloc((float**)&d_C, vecSizeInBytes);
+
+    /* Data transfer from host to device */
+    cudaMemcpy(d_A, h_A, vecSizeInBytes, cudaMemcpyHostToDevice);
+    cudaMemcpy(d_B, h_B, vecSizeInBytes, cudaMemcpyHostToDevice);
+    cudaMemcpy(d_C, devicePtr, vecSizeInBytes, cudaMemcpyHostToDevice);
+
+    /* Organizing grids and blocks */
+    int numThreadsInBlocks = 1024;
+    dim3 block (numThreadsInBlocks);
+    dim3 grid  ((vecSize + block.x - 1) / block.x);
+
+    /* Execute the kernel from the host*/
+    tStart = chronometer();
+    arraySumOnDevice<<<grid, block>>>(d_A, d_B, d_C, vecSize);
+    cudaDeviceSynchronize();
+    tElapsed = chronometer() - tStart;
+    printf("Elapsed time for arraySumOnDevice <<< %d, %d >>>: %f second(s)\n\n", \
+    grid.x, block.x, tElapsed);
+/*-----------------------------------------------*/
+    /* Returning the last error from a runtime call */
+    cudaGetLastError();
+
+    /* Data transfer back from device to host */
+    cudaMemcpy(devicePtr, d_C, vecSizeInBytes, cudaMemcpyDeviceToHost);
+
+    /* Check to see if the array summations on 
+     * CPU and GPU yield the same results 
+     */
+    arrayEqualityCheck(hostPtr, devicePtr, vecSize);
+/*-----------------------------------------------*/
+    /* Free the allocated memory on the device */
+    cudaFree(d_A);
+    cudaFree(d_B);
+    cudaFree(d_C);
+
+    /* Free the allocated memory on the host */
+    free(h_A);
+    free(h_B);
+    free(hostPtr);
+    free(devicePtr);
+
+    return(EXIT_SUCCESS);
+}
